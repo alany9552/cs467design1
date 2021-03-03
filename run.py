b0bda21b1ded061888f1fa0f7d2079012b601be7
Alan <alansmac@AlandeMacBook-Pro.local>
2021-03-03 17:05:01 -0600

    data query added

---
+++ run.py
@@ -0,0 +1,25 @@
+from chatlog.analysis.collectivity import Collectivity
+from chatlog.analysis.individual import Individual
+from chatlog.analysis.interesting import Interesting
+from chatlog.base.read_chatlog import ReadChatlog
+from chatlog.base.user_profile import UserProfile
+
+if __name__ == '__main__':
+    RC = ReadChatlog('./chatlog.txt')
+    RC.work()  # 进行聊天记录的清洗并入库
+    UP = UserProfile()
+    UP.work()  # 构建简单的用户画像
+
+    # Collectivity
+    # col = Collectivity()
+    # print(col.get_all_speak_info())  # 群聊天时间分布
+
+    # Individual
+    # ind = Individual()
+    # print(ind.longest_ban())  # 禁言时长的排名
+    # print(ind.most_speak('speak_num'))  # 发言次数的排名
+
+    # Interesting
+    # interest = Interesting()
+    # print(interest.longest_formation())  # 最长队形的排名
+    # print(interest.longest_name())  #  最长的马甲排名

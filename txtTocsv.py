8e2704faea3b1ddfc7674cced134de0b6944777f
Alan <alansmac@AlandeMacBook-Pro.local>
2021-03-03 17:31:44 -0600

    query csv version

--- txtTocsv.py
+++ txtTocsv.py
@@ -1,5 +1,25 @@
 import pandas as pd
+import csv
+csv_columns = ['time','ID','name', 'text']
 
-df = pd.read_csv("file.txt",delimiter="\n")
+f = open('dataquery.txt', 'r', encoding='utf-8')
+list = f.readlines()
+dictList = []
+for i in list:
+    dictList.append(eval(i))
 
-df.to_csv("test3.csv", encoding='utf-8', index=False)
+print(dictList)
+
+csv_file = "dataquery.csv"
+try:
+    with open(csv_file, 'w', encoding='utf-8-sig') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for data in dictList:
+            writer.writerow(data)
+except IOError:
+    print("I/O error")
+
+# df = pd.read_csv("dataquery.txt",delimiter="\n")
+#
+# df.to_csv("test3.csv", encoding='utf-8', index=False)
